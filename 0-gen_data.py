47e0f21fe5fcbf3ed59f88387aad228e21bcf8e2
Nicolai Amin <nicolaiamin1@gmail.com>
2024-04-24 12:36:51 +0200

    Data Gen Done

--- 0-gen_data.py
+++ 0-gen_data.py
@@ -14,16 +14,108 @@ with open(template, "r") as file:
 if not os.path.exists(mol):
     os.makedirs(mol)
 
-Type = "Band"
-# Type = "Conv_E"
-# Type = "Conv_V"
-# Type = "Conv_K"
-# Type = "2D"
+#Type = "Band"
+Type = "Conv_E"
+#Type = "Conv_V"
+#Type = "Conv_K"
+#Type = "2D"
 
 if Type == "Band":
     k = 6
     ecut = 600
     v = 6
+    n = 101
+    template_copy = template[:]
+    template_copy[6] = template_copy[6].replace("ECUT", str(ecut))
+    template_copy[7] = template_copy[7].replace("KDENS", str(k))
+    template_copy[8] = template_copy[8].replace("VACUUM", str(v))
+    template_copy[9] = template_copy[9].replace("FOLDER", "Band")
+    template_copy[10] = template_copy[10].replace("TYPE", "Band")
+    template_copy[11] = template_copy[11].replace("NPOINTS", str(n))
+    template_copy[13] = template_copy[13].replace("XYZ", xyz)
     
-    
+    with open("{}/Band.py".format(mol), "w") as file:
+        file.writelines(template_copy)
+        file.close()
+    with open("{}/Band.sh".format(mol), "w") as file:
+        file.write("#!/bin/bash\n")
+        file.write("mq submit Band.py -R 12:1d\n")
+        file.close()
 
+elif Type == "Conv_E":
+    if not os.path.exists("{}/Conv_E".format(mol)):
+        os.makedirs("{}/Conv_E".format(mol))
+    k = 6
+    v = 6
+    E = np.linspace(50,1000,20)
+    n = 31
+    for i in range(len(E)):
+        template_copy = template[:]
+        template_copy[6] = template_copy[6].replace("ECUT", str(E[i]))
+        template_copy[7] = template_copy[7].replace("KDENS", str(k))
+        template_copy[8] = template_copy[8].replace("VACUUM", str(v))
+        template_copy[9] = template_copy[9].replace("FOLDER", "Conv_E/Conv_E_{}".format(E[i]))
+        template_copy[10] = template_copy[10].replace("TYPE", "Conv")
+        template_copy[11] = template_copy[11].replace("NPOINTS", str(n))
+        template_copy[13] = template_copy[13].replace("XYZ", xyz)
+        
+        with open("{}/Conv_E/Conv_E_{}.py".format(mol,E[i]), "w") as file:
+            file.writelines(template_copy)
+            file.close()
+        with open("{}/Conv_E.sh".format(mol), "a") as file:
+            if i == 0:
+                file.write("#!/bin/bash\n")
+            file.write("mq submit Conv_E/Conv_E_{}.py -R 12:1d\n".format(E[i]))
+            file.close()
+        
+elif Type == "Conv_V":
+    if not os.path.exists("{}/Conv_V".format(mol)):
+        os.makedirs("{}/Conv_V".format(mol))
+    k = 6
+    v = np.linspace(1,7,13)
+    ec = 600
+    n = 31
+    for i in range(len(v)):
+        template_copy = template[:]
+        template_copy[6] = template_copy[6].replace("ECUT", str(ec))
+        template_copy[7] = template_copy[7].replace("KDENS", str(k))
+        template_copy[8] = template_copy[8].replace("VACUUM", str(v[i]))
+        template_copy[9] = template_copy[9].replace("FOLDER", "Conv_V/Conv_V_{}".format(v[i]))
+        template_copy[10] = template_copy[10].replace("TYPE", "Conv")
+        template_copy[11] = template_copy[11].replace("NPOINTS", str(n))
+        template_copy[13] = template_copy[13].replace("XYZ", xyz)
+        
+        with open("{}/Conv_V/Conv_V_{}.py".format(mol,v[i]), "w") as file:
+            file.writelines(template_copy)
+            file.close()
+        with open("{}/Conv_V.sh".format(mol), "a") as file:
+            if i == 0:
+                file.write("#!/bin/bash\n")
+            file.write("mq submit Conv_V/Conv_V_{}.py -R 12:1d\n".format(v[i]))
+            file.close()
+
+elif Type == "Conv_K":
+    if not os.path.exists("{}/Conv_K".format(mol)):
+        os.makedirs("{}/Conv_K".format(mol))
+    k = np.linspace(2,8,13)
+    v = 6
+    ec = 600
+    n = 31
+    for i in range(len(k)):
+        template_copy = template[:]
+        template_copy[6] = template_copy[6].replace("ECUT", str(ec))
+        template_copy[7] = template_copy[7].replace("KDENS", str(k[i]))
+        template_copy[8] = template_copy[8].replace("VACUUM", str(v))
+        template_copy[9] = template_copy[9].replace("FOLDER", "Conv_K/Conv_K_{}".format(k[i]))
+        template_copy[10] = template_copy[10].replace("TYPE", "Conv")
+        template_copy[11] = template_copy[11].replace("NPOINTS", str(n))
+        template_copy[13] = template_copy[13].replace("XYZ", xyz)
+        
+        with open("{}/Conv_K/Conv_K_{}.py".format(mol,k[i]), "w") as file:
+            file.writelines(template_copy)
+            file.close()
+        with open("{}/Conv_K.sh".format(mol), "a") as file:
+            if i == 0:
+                file.write("#!/bin/bash\n")
+            file.write("mq submit Conv_K/Conv_K_{}.py -R 12:1d\n".format(k[i]))
+            file.close()    
---
+++ VBr2/Conv_V.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mq submit Conv_V/Conv_V_1.0.py -R 12:1d
+mq submit Conv_V/Conv_V_1.5.py -R 12:1d
+mq submit Conv_V/Conv_V_2.0.py -R 12:1d
+mq submit Conv_V/Conv_V_2.5.py -R 12:1d
+mq submit Conv_V/Conv_V_3.0.py -R 12:1d
+mq submit Conv_V/Conv_V_3.5.py -R 12:1d
+mq submit Conv_V/Conv_V_4.0.py -R 12:1d
+mq submit Conv_V/Conv_V_4.5.py -R 12:1d
+mq submit Conv_V/Conv_V_5.0.py -R 12:1d
+mq submit Conv_V/Conv_V_5.5.py -R 12:1d
+mq submit Conv_V/Conv_V_6.0.py -R 12:1d
+mq submit Conv_V/Conv_V_6.5.py -R 12:1d
+mq submit Conv_V/Conv_V_7.0.py -R 12:1d
